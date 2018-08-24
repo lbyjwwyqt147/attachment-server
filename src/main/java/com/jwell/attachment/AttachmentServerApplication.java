24177c9f81121896a0d39c307eb7771efd6c793e
lbyjwwyqt147 <lbyjwwyqt147@163.com>
2018-08-24 17:37:35 +0800

    初始化

---
+++ src/main/java/com/jwell/attachment/AttachmentServerApplication.java
@@ -0,0 +1,12 @@
+package com.jwell.attachment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttachmentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttachmentServerApplication.class, args);
+    }
+}
